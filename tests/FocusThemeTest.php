d4cf3cdb76b80005447eb092eb36412160b12bc5
Ivo Petkov <ivo@ivopetkov.com>
2021-12-03 12:54:51 +0200

    Add unit tests.

---
+++ tests/FocusThemeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Focus Theme addon for Bear CMS
+ * https://github.com/bearcms/focus-theme-addon
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class FocusThemeTest extends BearCMS\AddonTests\PHPUnitTestCase
+{
+    /**
+     * 
+     */
+    public function testTheme()
+    {
+        $app = $this->getApp();
+
+        $this->assertTrue(true);
+    }
+}
